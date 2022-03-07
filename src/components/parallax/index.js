abd4221e3204d800de49d19c2c86832c86427a89
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-03-06 19:03:22 -0800

    started to work on the parallax

---
+++ src/components/parallax/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./style.css"
+
+
+function Parallax(){
+    return(
+        <>
+        <img src={"https://media3.giphy.com/media/3ohs873OdgNQGu017G/giphy.gif?cid=790b7611905eb2f36b4f24c07327b4842a94867893f4e306&rid=giphy.gif&ct=g"} alt="image of the world"/>
+        </>
+    )
+}
+
+export default Parallax;
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react"
-import {Button, FormControl, InputGroup,Form,Row, Col} from "react-bootstrap"
 import moment from "moment-timezone"
+import Parallax from "../components/parallax"
 
 class Landing extends Component{
     state   = {
         currenTime: "",
         currentCity:"",
-        currentContinent:"",
-        cityLivedIn:[],
-        cityToSendWork:[]
 
     }
     componentDidMount(){
@@ -18,65 +15,19 @@ class Landing extends Component{
     getCurrentTime = () =>{
         this.setState({ currenTime : moment().format('h:mm:ss a')})
     }
-    searchCitySearchButton = (event) =>{
-        event.preventDefault()
-        this.getTime(this.state.currentCity,this.state.currentContinent)
-
-    }
-    searchCityEnter = event =>{
-        if(event.key === 'Enter'){
-            console.log("Enter pressed")
-            this.getTime(this.state.currentCity,this.state.currentContinent)
-        }
-    }
     handleInputChange = event =>{
         const {name,value} = event.target;
         this.setState({
             [name]: value
         })
     }
-    getTime = (city, continent)=>{
-        console.log(moment().tz(`${continent}/${city}`).format('h:mma z'))
-        console.log("Moments Returned")
-    }
     render(){
         return(
         <div>
+             <Parallax />
             <div className="text-center pt-4">
             <h1 name="currentTime">Your Current Time: {this.state.currenTime}</h1>
             </div>
-            <Row className="d-flex justify-content-center pt-3">
-                <Col sm={6}>
-                <InputGroup className="mb-5" >
-                    <FormControl
-                    name="currentCity"
-                    placeholder="City to look for"
-                    value = {this.state.currentCity}
-                    onKeyDown={this.searchCityEnter}
-                    onChange={this.handleInputChange}
-                    />
-                    <Form.Select 
-                    aria-label="Select Continent" 
-                    size="sm"
-                    onChange={this.handleInputChange}
-                    value={this.state.currentContinent}
-                    name="currentContinent"
-                    >
-                        <option>Select Continent</option>
-                        <option value="America">America</option>
-                        <option value="Europe">Europe</option>
-                        <option value="Africa">Africa</option>
-                        <option value="Asia">Asia</option>
-                        <option value="Indian">Indian</option>
-                        <option value="Australia">Australia</option>
-                    </Form.Select>
-                    <Button variant="outline-secondary" id="button-addon2" onClick={this.searchCitySearchButton}>
-                    Search
-                    </Button>
-                </InputGroup>
-                </Col>
-            </Row>
-
         </div>
         )
     }
