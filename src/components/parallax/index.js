bf91657f531e1a47a2216436357a48f3676f0c59
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-03-12 21:10:25 -0800

    finally fixed parallax

--- src/components/parallax/index.js
+++ src/components/parallax/index.js
@@ -3,9 +3,19 @@ import "./style.css"
 
 
 function Parallax(){
+    const parallax = document.getElementById("mainParallaxImg");
+    window.addEventListener("scroll",function(){
+        let offset = window.pageYOffset;
+        console.log('Offset:' + offset)
+        parallax.style.backgroundPositionY  = offset * 0.7 +"px";
+    })
+
+
     return(
         <>
-        <img src={"https://media3.giphy.com/media/3ohs873OdgNQGu017G/giphy.gif?cid=790b7611905eb2f36b4f24c07327b4842a94867893f4e306&rid=giphy.gif&ct=g"} alt="image of the world"/>
+        <div>
+            <span id="mainParallaxImg"></span>
+        </div>
         </>
     )
 }
