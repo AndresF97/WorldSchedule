b74b389d77f428b00f864711fb82e3b0e70bffd2
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-03-28 13:42:50 -0700

    got chartjs installed and got it to display

--- src/pages/projectsPage.js
+++ src/pages/projectsPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Col, Row,Card,ProgressBar,ListGroup,Form } from "react-bootstrap";
+import {Line} from "react-chartjs-2";
+import Chart from "chart.js/auto"
+import { Col, Row,Card,ProgressBar} from "react-bootstrap";
 import moment from "moment-timezone"
-
 
 
 
@@ -9,13 +10,24 @@ class projectsPage extends Component{
     //Setting state
     state={
         currentTime:"",
+        dummyData:{
+            labels:["Date1","Date2","Date3","Date4","Date5","Date6"],
+            datasets:[
+                {
+                    label:"Time worked",
+                    data:[1,2,3,4,5,6],
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)'
+                }
+            ]
+        },
         projectName:"Your Project Name",
         currentProgress:60
     }
+
     //Component Mounted
     componentDidMount(){
         setInterval(()=> this.getCurrentTime(),1000)
-
     }
     getCurrentTime= ()=>{
         this.setState({currentTime: moment().format('h:mm:ss a')})
@@ -28,21 +40,21 @@ class projectsPage extends Component{
             <h3 className="text-center">{this.state.projectName}</h3>
             <ProgressBar style={{width:'50%',display:'block',marginLeft:'auto',marginRight:'auto',textAlign:"center"}}animated now={this.state.currentProgress} label={`${this.state.currentProgress}%`} />
             <Row>
-                <Col className="mr-1">
+                <Col className="m-5">
                 <Card style={{ width: '18rem' }}>
                     <Card.Body>
                         <Card.Title>Todo List:</Card.Title>
                         <Card.Text>
-                        <ListGroup>
-                            <ListGroup.Item><Form.Check type="checkbox" label="Check me out" /> </ListGroup.Item>
-                            <ListGroup.Item><Form.Check type="checkbox" label="Check me out" /> </ListGroup.Item>
-                            <ListGroup.Item><Form.Check type="checkbox" label="Check me out" /> </ListGroup.Item>
-                            <ListGroup.Item><Form.Check type="checkbox" label="Check me out" /> </ListGroup.Item>
-                            <ListGroup.Item><Form.Check type="checkbox" label="Check me out" /> </ListGroup.Item>
-                        </ListGroup>
                         </Card.Text>
                     </Card.Body>
                 </Card>
+                </Col>
+                <Col>
+                    <h1 className="text-center">Project Time remaining</h1>
+                    <Line
+                    className="m-2"
+                    data={this.state.dummyData}
+                    />
                 </Col>
             </Row>
         </div>
