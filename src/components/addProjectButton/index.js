ed1c72fddfb2419cedcd9734c99e145c9b5396b5
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-03-28 22:03:07 -0700

    Added project button modal, next gotta work on drop down

--- src/components/addProjectButton/index.js
+++ src/components/addProjectButton/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
+import {Modal,Button} from "react-bootstrap"
 import "./style.css";
 
 function AddProjectButton(props){
+    const [show,setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return(
         <div>
-            <button id="addButton" onClick={props.showFunction}>
+            <button id="addButton"  onClick={handleShow}>
                 +
             </button>
             {/* AS SOON AS THE BUTTON IS CLICKED IT SHOULD DISPLAY THE TIME SELECTION AND PORJECT NAME */}
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Enter Project info</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Submit
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
